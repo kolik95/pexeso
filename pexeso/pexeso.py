d0e576973462890bdb16bdae178d5798221598f1
kolik95 <ivankotalik@seznam.cz>
2023-04-16 21:21:59 +0200

    back button

--- pexeso/pexeso.py
+++ pexeso/pexeso.py
@@ -7,15 +7,24 @@ from collections.abc import Callable
 class Pexeso(Scene):
     def __init__(self, gap:float, rect_size:float, width:int, height:int, scene_switch:Callable[[int], None]):
         super().__init__(scene_switch)
-
-        self.screen_objects = [[[] for _ in range((width-gap)//(rect_size+gap))] for _ in range((height-gap)//(rect_size+gap))]
+        left_offset = 30
+        top_offset = 30
+        self.screen_objects = [[[] for _ in range((width-gap-left_offset)//(rect_size+gap))] for _ in range((height-gap-top_offset)//(rect_size+gap))]
         for i in range(len(self.screen_objects)):
             for j in range(len(self.screen_objects[i])):
-                self.screen_objects[i][j] = [Rect(gap + j * (rect_size+gap), gap + i * (rect_size+gap), rect_size, rect_size), "blue"]
-        self.pos = None
+                self.screen_objects[i][j] = [Rect(left_offset + gap + j * (rect_size+gap), top_offset + gap + i * (rect_size+gap), rect_size, rect_size), "blue", self.change_color]
+        self.screen_objects[0].append([Rect(0, 0, 50, 50), "blue", self.open_menu])
 
     def on_mouse_down(self, pos):
-        self.pos = pos
+        if pos:
+            for x in self.screen_objects:
+                for y in x:
+                    if y[0].collidepoint(pos):
+                        y[2](y)
+                        break
+                else:
+                    continue
+                break
 
     def draw(self, screen):
         screen.fill("white")
@@ -24,13 +33,10 @@ class Pexeso(Scene):
                 draw.rect(screen, y[1], y[0])
 
     def update(self, mouse_pos):
-        if self.pos:
-            for x in self.screen_objects:
-                for y in x:
-                    if y[0].collidepoint(self.pos):
-                        y[1] = "red"
-                        self.pos = None
-                        break
-                else:
-                    continue
-                break
+        pass
+
+    def change_color(self, rect_struct):
+        rect_struct[1] = "red"
+
+    def open_menu(self, y):
+        self.switch_scenes(0)
