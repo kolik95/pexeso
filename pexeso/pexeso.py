2aad4ac86999ba39e98990e79454224d9c59e933
kolik95 <ivankotalik@seznam.cz>
2023-04-17 21:16:23 +0200

    fix counting

--- pexeso/pexeso.py
+++ pexeso/pexeso.py
@@ -24,7 +24,7 @@ class Pexeso(Scene):
         shuffle(pairs)
         for i in range(8):
             for j in range(8):
-                self.screen_objects[i*8 + j] = PexesoRect(left_offset + j * (rect_width+gap), top_offset + i * (rect_height+gap), rect_width, rect_height, pairs[i*8+j], self.on_pexeso_click, lambda x: None, lambda x: None, "blue", "black", str(pairs[i*8 + j]+ 1), self.button_font)
+                self.screen_objects[i*8 + j] = PexesoRect(left_offset + j * (rect_width+gap), top_offset + i * (rect_height+gap), rect_width, rect_height, pairs[i*8+j], self.on_pexeso_click, lambda x: None, lambda x: None, "blue", "black", str(pairs[i*8 + j]), self.button_font)
         self.screen_objects.append(TextRect(10, 10, 95, 50, "blue", "white", "Zpět", self.button_font, self.open_menu, self.on_button_hover, self.on_button_hover_leave))
         self.screen_objects.append(TextRect(10, 70, 95, 50, "blue", "white", "Znovu", self.button_font, self.reset, self.on_button_hover, self.on_button_hover_leave))
 
